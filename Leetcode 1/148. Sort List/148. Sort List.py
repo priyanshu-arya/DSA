910660f28d59244509c47801e360828d8a292c45
priyanshu-arya <priyanshuarya2482000@gmail.com>
2022-02-24 22:59:45 +0530

    24 Feb 2022

---
+++ Leetcode 1/148. Sort List/148. Sort List.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/sort-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.sortListTopDown(head)
+
+    def sortListTopDown(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        mid = self.getmidlist(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+
+    def merge(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:
+            return list1 or list2
+
+        if list1.val < list2.val:
+            list1.next = self.merge(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge(list1, list2.next)
+            return list2
+
+    def getmidlist(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head.next, head
+
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+
+        start = slow.next
+        slow.next = None
+        return start
